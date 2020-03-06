42001ec9486bc8bfd3ba3374637c8afadc8de07d
odicchi <ynishiyama.bis@gmail.com>
2020-03-06 10:42:16 +0900

    Add CheckEffects Interactions Pattern

---
+++ CheckEffectsInteractionsPattern/ChecksEffectsInteractions.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.5.16;
+
+contract ChecksEffectsInteractions {
+
+    mapping(address => uint) balances;
+
+    function deposit() public payable {
+        balances[msg.sender] = msg.value;
+    }
+
+    function withdraw(uint amount) public {
+        require(balances[msg.sender] >= amount);
+
+        balances[msg.sender] -= amount;
+
+        msg.sender.transfer(amount);
+    }
+}
